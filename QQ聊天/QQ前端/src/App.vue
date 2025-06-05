3450b0ab87e89cc6836e0fd472093d98e9bb1c7b
Louioskarmen <ayxc000@qq.com>
2025-06-05 19:58:42 +0800

    6.5.1(开始three.js)

--- QQ聊天/QQ前端/src/App.vue
+++ QQ聊天/QQ前端/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <Routerview></Routerview>
+  </div>
 </template>
 
-<script setip lang="ts"></script>
+<script setip lang="ts" name="App">
+
+
+</script>
 
 <style scoped lang="scss"></style>
